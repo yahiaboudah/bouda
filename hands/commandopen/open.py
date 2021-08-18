e1d60c8d9acc022f778599377c6bfb1ab0157d9c
yahiaboudah <yahiabouda@hotmail.com>
2021-08-18 11:46:39 +0100

    creating a program opener class

--- hands/commandopen/open.py
+++ hands/commandopen/open.py
@@ -5,44 +5,55 @@ import subprocess
 
 PROGRAMS = "programs.json"
 
-def open_msg(name):
-    return f"=== opening {name} ==="
-
-def fail_msg(name):
-    return f"program {name} not recognized."
-
-def get_programs(psp):
-    with open(psp) as ps:
-        ps = json.loads(ps.read())
-    return ps
-
-def cli():
-    my_parser = argparse.ArgumentParser(prog= 'open',
-                                    description= 'open a program from the command line',
-                                    usage='%(prog)s program_name')
-
-    my_parser.add_argument('-p', '--program', action='store', type=str, required=False)
-
-    args = my_parser.parse_args()
-
-    return vars(args)
-
-def open_program(process, my_programs):
+class ProgramOpener():
     
-    if(process in my_programs):
-
-        data = my_programs[process].split('-')
-        pr, msg = data[0], open_msg(data[1])
-
-        subprocess.Popen(pr)
-        return msg
+    def __init__(self, programs_dir):
+        self.passed_pr_name = None 
+        self.programs_dir   = programs_dir
     
-    else: return fail_msg(process)
+    def open_msg(self, name):
+        return f"=== opening {name} ==="
+
+    def fail_msg(self, name):
+        return f"Program {name} not recognized."
+
+    def get_programs(self):
+        with open(self.programs_dir) as ps:
+            ps = json.loads(ps.read())
+        return ps
+
+    def cli(self):
+        my_parser = argparse.ArgumentParser(prog= 'open',
+                                        description= 'open a program from the command line',
+                                        usage='%(prog)s program_name')
+
+        my_parser.add_argument('-p', '--program', action='store', type=str, required=False)
+
+        args = my_parser.parse_args()
+
+        self.passed_pr_name = vars(args)["program"]
+    
+    def run_cli(self):
+        self.cli()
+        return self.open()
+
+    def open(self, process = None):
+
+        my_programs = self.get_programs()
+        if(process is None): process = self.passed_pr_name
+
+        if(process in my_programs):
+
+            data = my_programs[process].split('-')
+            pr, msg = data[0], self.open_msg(data[1])
+
+            subprocess.Popen(pr)
+            return msg
+        
+        else: return self.fail_msg(process)
 
 
 if __name__ == "__main__":
-    a  = cli()
-    p  = a["program"]
-    ps = get_programs(PROGRAMS)
-    rs = open_program(p, ps)
+    po = ProgramOpener(PROGRAMS)
+    rs = po.run_cli()
     print(rs)
