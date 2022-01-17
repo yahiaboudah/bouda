e4447d0714ad3ecb0504c6bb919de62c5c4a34bf
yahia boudah <yahiabouda@hotmail.com>
2022-01-17 14:32:37 +0100

    add processing function

--- app.py
+++ app.py
@@ -8,18 +8,22 @@ from flask import request
 from datetime import datetime as dt
 import pandas as pd
 
-data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
-series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
+def king_data():
+    data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
+    series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
 
-for m in data.index:
-    series.loc[data.loc[m].s:data.loc[m].e] = m
+    for m in data.index:
+        series.loc[data.loc[m].s:data.loc[m].e] = m
+    
+    return series.loc
 
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    data = king_data()
     year = int(request.args['shit'])
     try:
-        return series.loc[year]
+        return data[year]
     except KeyError:
         return f'Invalid input ({series.index.min()} - {series.index.max()})'
